7c98038995226e98feae346bd7b8c12093b2bc8d
Lisandro Dalcin <dalcinl@gmail.com>
2024-05-25 10:11:13 +0300

    recipe: Update test script for Open MPI v5.x and add more import tests

--- recipe/test_mpi4py.py
+++ recipe/test_mpi4py.py
@@ -1,14 +1,17 @@
 import os
 
 import mpi4py
-from mpi4py import MPI
 import mpi4py.futures
+import mpi4py.util.dtlib
+import mpi4py.util.pkl5
+from mpi4py import MPI
 
 # on Windows this checks MSMPI_VER
 print(MPI.get_vendor())
 
 print(MPI.Get_library_version())
 
+# TODO: remove for mpi4py 4.0.0
 # verify get_config paths exist (not links to build env)
 config = mpi4py.get_config()
 print(f"config={config}")
