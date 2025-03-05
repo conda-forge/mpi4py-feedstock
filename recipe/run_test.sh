207fc255265f0a882d812b6d26d2c3a8ba7a1bcc
Min RK <benjaminrk@gmail.com>
2025-03-05 14:42:10 +0100

    try UCX_HANDLE_ERRORS=none

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,7 +4,8 @@ set -ex
 # UCX memory events can cause crashes,
 # at least on linux-aarch64 + CPython 3.12
 # also seen in Julia: https://github.com/openucx/ucx/issues/5061
-export UCX_MEM_EVENTS=no
+# export UCX_MEM_EVENTS=no
+export UCX_HANDLE_ERRORS=none
 
 # Basic tests
 mpiexec -n 1 python -m mpi4py --version
